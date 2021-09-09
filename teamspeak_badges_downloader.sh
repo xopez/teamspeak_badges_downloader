e1c06492c2d096d4629a19f4dc85650768607f5c
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-09-10 01:34:03 +0200

    Include downloadlink in json

--- teamspeak_badges_downloader.sh
+++ teamspeak_badges_downloader.sh
@@ -14,11 +14,19 @@ grep -oP 'http.?://\S+\"' "$TMPDIR"/teamspeak_badges_downloader | sed 's/.$//' |
 
 # genereate csv
 echo "Generating CSV files"
-echo "id;filename" > "$PUB"/id_filelist.csv
-sed 's/https:\/\/badges-content.teamspeak.com\///' "$TMPDIR"/teamspeak_badges_downloadlist.txt |sed 's/\//;/g' >> "$PUB"/id_filelist.csv
+
+# Part 1
+echo "id;filename;downloadlink" > "$PUB"/id_filelist.csv
+sed 's/https:\/\/badges-content.teamspeak.com\///' "$TMPDIR"/teamspeak_badges_downloadlist.txt |sed 's/\//;/g' > "$TMPDIR"/id_filelist.txt
+paste -d ";" "$TMPDIR"/id_filelist.txt "$TMPDIR"/teamspeak_badges_downloadlist.txt > "$TMPDIR"/id_filelist.csv
+cat "$TMPDIR"/id_filelist.csv >> "$PUB"/id_filelist.csv
 [ -z "$(tail -c1 "$PUB"/id_filelist.csv)" ] && truncate -s -1 "$PUB"/id_filelist.csv
-echo "id;filename" > "$PUB"/id_filelist_details.csv
-sed 's/https:\/\/badges-content.teamspeak.com\///' "$TMPDIR"/teamspeak_badges_details_downloadlist.txt | sed 's/\//;/g' >> "$PUB"/id_filelist_details.csv
+
+# Part 2
+echo "id;filename;downloadlink" > "$PUB"/id_filelist_details.csv
+sed 's/https:\/\/badges-content.teamspeak.com\///' "$TMPDIR"/teamspeak_badges_details_downloadlist.txt | sed 's/\//;/g' > "$TMPDIR"/id_filelist_details.txt
+paste -d ";" "$TMPDIR"/id_filelist_details.txt "$TMPDIR"/teamspeak_badges_details_downloadlist.txt > "$TMPDIR"/id_filelist_details.csv
+cat "$TMPDIR"/id_filelist_details.txt >> "$PUB"/id_filelist_details.csv
 [ -z "$(tail -c1 "$PUB"/id_filelist_details.csv)" ] && truncate -s -1 "$PUB"/id_filelist_details.csv
 
 # generate json
@@ -26,7 +34,8 @@ echo "Generating JSON files"
 jq --slurp --raw-input --raw-output \
     'split("\n") | .[1:] | map(split(";")) |
         map({"id": .[0],
-        "filename": .[1]})' \
+        "filename": .[1],
+        "downloadlink": .[2]})' \
         "$PUB"/id_filelist.csv > "$PUB"/id_filelist.json
 
 jq --slurp --raw-input --raw-output \
