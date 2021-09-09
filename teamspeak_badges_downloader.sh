c02fb400e9f7e949477c1017d06bec62574ea5fc
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-09-09 20:23:52 +0200

    Reducing output

--- teamspeak_badges_downloader.sh
+++ teamspeak_badges_downloader.sh
@@ -3,21 +3,25 @@ DIR=/root/teamspeak_tmp
 PUB=/var/www/fs.mightful-noobs.de/teamspeak_badges
 
 # download
+echo "Downloading the link list"
 curl https://badges-content.teamspeak.com/list > "$DIR"/teamspeak_badges_downloader
 
-# grep downloadlinks
-grep -oP 'http.?://\S+\"' "$DIR"/teamspeak_badges_downloader | sed 's/.$//' | sed -e 's/$/.svg/' | tr -d '"' | tee "$PUB"/teamspeak_badges_downloader.list
-grep -oP 'http.?://\S+\"' "$DIR"/teamspeak_badges_downloader | sed 's/.$//' | sed -e 's/$/_details.svg/' | tr -d '"' | tee "$PUB"/teamspeak_badges_downloader_details.list
+# grab downloadlinks
+echo "Grabbing Download links"
+grep -oP 'http.?://\S+\"' "$DIR"/teamspeak_badges_downloader | sed 's/.$//' | sed -e 's/$/.svg/' | tr -d '"' > "$PUB"/teamspeak_badges_downloader.list
+grep -oP 'http.?://\S+\"' "$DIR"/teamspeak_badges_downloader | sed 's/.$//' | sed -e 's/$/_details.svg/' | tr -d '"' > "$PUB"/teamspeak_badges_downloader_details.list
 
 # genereate csv
-echo "id;filename" | tee "$PUB"/id_filelist.csv
-sed 's/https:\/\/badges-content.teamspeak.com\///' "$PUB"/teamspeak_badges_downloader.list |sed 's/\//;/g' | tee -a "$PUB"/id_filelist.csv
+echo "Generating CSV files"
+echo "id;filename" > "$PUB"/id_filelist.csv
+sed 's/https:\/\/badges-content.teamspeak.com\///' "$PUB"/teamspeak_badges_downloader.list |sed 's/\//;/g' >> "$PUB"/id_filelist.csv
 [ -z "$(tail -c1 "$PUB"/id_filelist.csv)" ] && truncate -s -1 "$PUB"/id_filelist.csv
-echo "id;filename" | tee "$PUB"/id_filelist_details.csv
-sed 's/https:\/\/badges-content.teamspeak.com\///' "$PUB"/teamspeak_badges_downloader_details.list | sed 's/\//;/g' | tee -a "$PUB"/id_filelist_details.csv
+echo "id;filename" > "$PUB"/id_filelist_details.csv
+sed 's/https:\/\/badges-content.teamspeak.com\///' "$PUB"/teamspeak_badges_downloader_details.list | sed 's/\//;/g' >> "$PUB"/id_filelist_details.csv
 [ -z "$(tail -c1 "$PUB"/id_filelist_details.csv)" ] && truncate -s -1 "$PUB"/id_filelist_details.csv
 
 # generate json
+echo "Generating JSON files"
 jq --slurp --raw-input --raw-output \
     'split("\n") | .[1:] | map(split(";")) |
         map({"id": .[0],
@@ -35,15 +39,14 @@ cd "$PUB"/badges  || exit
 truncate -s 0 "$PUB"/filelist.txt
 truncate -s 0 "$PUB"/filelist_details.txt
 
+echo "Downloading Images..."
 while read url; do
-  echo "$url"
-  echo "${url##*/}" | tee -a "$PUB"/filelist.txt
+  echo "${url##*/}" >> "$PUB"/filelist.txt
   curl -sL -O "$url"
 done < "$PUB"/teamspeak_badges_downloader.list
 
 while read url; do
-  echo "$url"
-  echo "${url##*/}" | tee -a "$PUB"/filelist_details.txt
+  echo "${url##*/}" >> "$PUB"/filelist_details.txt
   curl -sL -O "$url"
 done < "$PUB"/teamspeak_badges_downloader_details.list
 
