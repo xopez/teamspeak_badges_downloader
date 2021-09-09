ac3b5676604398851bdd5109c975d5651e06815f
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-09-10 00:58:26 +0200

    Use tmp dir instead of root location
    the files doesn't need to survive a reboot, since they are not public and refreshed at running.

--- teamspeak_badges_downloader.sh
+++ teamspeak_badges_downloader.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-DIR=/root/teamspeak_tmp
+TMPDIR=/tmp
 PUB=/var/www/fs.mightful-noobs.de/teamspeak_badges
 
 # download
 echo "Downloading the link list"
-curl https://badges-content.teamspeak.com/list > "$DIR"/teamspeak_badges_downloader
+curl https://badges-content.teamspeak.com/list > "$TMPDIR"/teamspeak_badges_downloader
 
 # grab downloadlinks
 echo "Grabbing Download links"
-grep -oP 'http.?://\S+\"' "$DIR"/teamspeak_badges_downloader | sed 's/.$//' | sed -e 's/$/.svg/' | tr -d '"' > "$PUB"/teamspeak_badges_downloadlist.txt
-grep -oP 'http.?://\S+\"' "$DIR"/teamspeak_badges_downloader | sed 's/.$//' | sed -e 's/$/_details.svg/' | tr -d '"' > "$PUB"/teamspeak_badges_details_downloadlist.txt
+grep -oP 'http.?://\S+\"' "$TMPDIR"/teamspeak_badges_downloader | sed 's/.$//' | sed -e 's/$/.svg/' | tr -d '"' > "$TMPDIR"/teamspeak_badges_downloadlist.txt
+grep -oP 'http.?://\S+\"' "$TMPDIR"/teamspeak_badges_downloader | sed 's/.$//' | sed -e 's/$/_details.svg/' | tr -d '"' > "$TMPDIR"/teamspeak_badges_details_downloadlist.txt
 
 # genereate csv
 echo "Generating CSV files"
 echo "id;filename" > "$PUB"/id_filelist.csv
-sed 's/https:\/\/badges-content.teamspeak.com\///' "$PUB"/teamspeak_badges_downloadlist.txt |sed 's/\//;/g' >> "$PUB"/id_filelist.csv
+sed 's/https:\/\/badges-content.teamspeak.com\///' "$TMPDIR"/teamspeak_badges_downloadlist.txt |sed 's/\//;/g' >> "$PUB"/id_filelist.csv
 [ -z "$(tail -c1 "$PUB"/id_filelist.csv)" ] && truncate -s -1 "$PUB"/id_filelist.csv
 echo "id;filename" > "$PUB"/id_filelist_details.csv
-sed 's/https:\/\/badges-content.teamspeak.com\///' "$PUB"/teamspeak_badges_details_downloadlist.txt | sed 's/\//;/g' >> "$PUB"/id_filelist_details.csv
+sed 's/https:\/\/badges-content.teamspeak.com\///' "$TMPDIR"/teamspeak_badges_details_downloadlist.txt | sed 's/\//;/g' >> "$PUB"/id_filelist_details.csv
 [ -z "$(tail -c1 "$PUB"/id_filelist_details.csv)" ] && truncate -s -1 "$PUB"/id_filelist_details.csv
 
 # generate json
@@ -43,12 +43,12 @@ echo "Downloading Images..."
 while read url; do
   echo "${url##*/}" >> "$PUB"/filelist.txt
   curl -sL -O "$url"
-done < "$PUB"/teamspeak_badges_downloadlist.txt
+done < "$TMPDIR"/teamspeak_badges_downloadlist.txt
 
 while read url; do
   echo "${url##*/}" >> "$PUB"/filelist_details.txt
   curl -sL -O "$url"
-done < "$PUB"/teamspeak_badges_details_downloadlist.txt
+done < "$TMPDIR"/teamspeak_badges_details_downloadlist.txt
 
 # right owner
 chown -R www-data: "$PUB"
