5b9e2a24ed362395de1e66d741b0bbc06fb6981e
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-09-10 02:10:14 +0200

    Adding own downloadlink to file and resort some commands
    also comment a bit

--- teamspeak_badges_downloader.sh
+++ teamspeak_badges_downloader.sh
@@ -16,17 +16,29 @@ grep -oP 'http.?://\S+\"' "$TMPDIR"/teamspeak_badges_downloader | sed 's/.$//' |
 echo "Generating CSV files"
 
 # Part 1
-echo "id;filename;downloadlink" > "$PUB"/id_filelist.csv
+# get IDs and filelist for csv
 sed 's/https:\/\/badges-content.teamspeak.com\///' "$TMPDIR"/teamspeak_badges_downloadlist.txt |sed 's/\//;/g' > "$TMPDIR"/id_filelist.txt
+# add own domain
+awk '{$0=$0";https://fs.mightful-noobs.de/teamspeak_badges/badges/"}1' "$TMPDIR"/id_filelist.txt > "$TMPDIR"/tmp.txt
+paste -d "" "$TMPDIR"/tmp.txt "$TMPDIR"/filelist.txt > "$TMPDIR"/id_filelist.txt
+# add original download link
 paste -d ";" "$TMPDIR"/id_filelist.txt "$TMPDIR"/teamspeak_badges_downloadlist.txt > "$TMPDIR"/id_filelist.csv
+# make public and combine csv
+echo "id;filename;downloadlink;original_file" > "$PUB"/id_filelist.csv
 cat "$TMPDIR"/id_filelist.csv >> "$PUB"/id_filelist.csv
 [ -z "$(tail -c1 "$PUB"/id_filelist.csv)" ] && truncate -s -1 "$PUB"/id_filelist.csv
 
 # Part 2
-echo "id;filename;downloadlink" > "$PUB"/id_filelist_details.csv
-sed 's/https:\/\/badges-content.teamspeak.com\///' "$TMPDIR"/teamspeak_badges_details_downloadlist.txt | sed 's/\//;/g' > "$TMPDIR"/id_filelist_details.txt
+# get IDs and filelist for csv
+sed 's/https:\/\/badges-content.teamspeak.com\///' "$TMPDIR"/teamspeak_badges_details_downloadlist.txt |sed 's/\//;/g' > "$TMPDIR"/id_filelist_details.txt
+# add own domain
+awk '{$0=$0";https://fs.mightful-noobs.de/teamspeak_badges/badges/"}1' "$TMPDIR"/id_filelist_details.txt > "$TMPDIR"/tmp.txt
+paste -d "" "$TMPDIR"/tmp.txt "$TMPDIR"/filelist_details.txt > "$TMPDIR"/id_filelist_details.txt
+# add original download link
 paste -d ";" "$TMPDIR"/id_filelist_details.txt "$TMPDIR"/teamspeak_badges_details_downloadlist.txt > "$TMPDIR"/id_filelist_details.csv
-cat "$TMPDIR"/id_filelist_details.txt >> "$PUB"/id_filelist_details.csv
+# make public and combine csv
+echo "id;filename;downloadlink;original_file" > "$PUB"/id_filelist_details.csv
+cat "$TMPDIR"/id_filelist_details.csv >> "$PUB"/id_filelist_details.csv
 [ -z "$(tail -c1 "$PUB"/id_filelist_details.csv)" ] && truncate -s -1 "$PUB"/id_filelist_details.csv
 
 # generate json
@@ -35,13 +47,16 @@ jq --slurp --raw-input --raw-output \
     'split("\n") | .[1:] | map(split(";")) |
         map({"id": .[0],
         "filename": .[1],
-        "downloadlink": .[2]})' \
+        "download": .[2],
+        "original_file": .[3]})' \
         "$PUB"/id_filelist.csv > "$PUB"/id_filelist.json
 
 jq --slurp --raw-input --raw-output \
     'split("\n") | .[1:] | map(split(";")) |
         map({"id": .[0],
-        "filename": .[1]})' \
+        "filename": .[1],
+        "download": .[2],
+        "original_file": .[3]})' \
         "$PUB"/id_filelist_details.csv > "$PUB"/id_filelist_details.json
 
 # actual download of files
